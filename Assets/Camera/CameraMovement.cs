2dccd87f4a5c571e20f8ee6930ba9bc42f3139ff
Ziucay <justlumajustmail@gmail.com>
2020-06-12 18:22:43 +0300

    Some tasks implemented
    Created camera script
    Created enemy spawner
    Created Health script

---
+++ Assets/Camera/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //then we move camera by mouse(percentage)
+    [Range(0, 0.5f)]
+    public float heightTrigger;
+    [Range(0, 0.5f)]
+    public float widthTrigger;
+    Vector3 mouseMove;
+    Vector3 position;
+    Vector3 move;
+    [SerializeField]
+    private float speed;
+    void FixedUpdate()
+    {
+        move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),0) * speed;
+        mouseMove = new Vector3(0, 0, 0);
+        position = Input.mousePosition;
+        if (position.x <= Screen.width * widthTrigger)
+        {
+            mouseMove.x = -1;
+            Debug.Log("1");
+        }
+        else if (position.x >= Screen.width * (1 - widthTrigger))
+        {
+            mouseMove.x = 1;
+            Debug.Log("2");
+        }
+            
+        if (position.y <= Screen.height * heightTrigger)
+        {
+            mouseMove.y = -1;
+            Debug.Log(3);
+        }
+        else if (position.y >= Screen.height * (1 - heightTrigger))
+        {
+            mouseMove.y = 1;
+            Debug.Log(4);
+        }
+            
+        mouseMove = mouseMove * speed;
+        transform.position = transform.position + move + mouseMove;
+    }
+}
---
+++ Assets/EnemyCreator/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform[] spawnerPoints;
+    [SerializeField]
+    private GameObject[] enemies;
+    [SerializeField]
+    private float spawnRate; //delay between enemy spawn
+    [SerializeField]
+    private float spawnCount; //amount of enemy spawned in one time
+    void Start()
+    {
+        
+        for (int i = 0;i < spawnerPoints.Length;++i)
+        {
+            spawnerPoints[i] = transform.GetChild(i);
+        }
+        StartCoroutine("Spawn");
+    }
+    
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(spawnRate);
+        for (int i = 0;i < spawnCount;++i)
+        {
+            Instantiate(enemies[Random.Range(0, enemies.Length)],
+                spawnerPoints[Random.Range(0, spawnerPoints.Length)].position,
+                Quaternion.identity);
+        }
+        StartCoroutine("Spawn");
+    }
+}
