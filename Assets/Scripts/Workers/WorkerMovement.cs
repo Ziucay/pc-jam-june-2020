e96b92c39d35cacca6b43eb4790121c1c9581439
AlexDest <condor000123@gmail.com>
2020-06-13 19:59:05 +0300

    MinorChanges + Worker
    Worker not implemented

---
+++ Assets/Scripts/Workers/WorkerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerMovement : MonoBehaviour
+{
+    private bool _isSelect = false;
+    [SerializeField] private float speed = 10f;
+    private Vector3 _newPosition;
+    private float _error;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isSelect)
+        {
+            Move();
+        }
+    }
+
+    public bool IsSelect => _isSelect;
+
+    public void Select(bool value)
+    {
+        _isSelect = value;
+    }
+
+    private void Move()
+    {
+        Vector3 direction = _newPosition - transform.position;
+        
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+        if (transform.position.x + _error <= _newPosition.x 
+            && transform.position.y + _error <= _newPosition.x)
+        {
+            _isSelect = false;
+        }
+    }
+}
