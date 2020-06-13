4f6825c907766b8032008c3146109c815437f18c
Ziucay <justlumajustmail@gmail.com>
2020-06-13 12:23:12 +0300

    Minor changes
    Camera modification
    New scene created

--- Assets/Camera/CameraMovement.cs
+++ Assets/Camera/CameraMovement.cs
@@ -14,33 +14,45 @@ public class CameraMovement : MonoBehaviour
     Vector3 move;
     [SerializeField]
     private float speed;
+    [SerializeField,Range(0,3)]
+    private float zoomSpeed;
+    [SerializeField]
+    private int ZoomBoundLeft;
+    [SerializeField]
+    private int ZoomBoundRight;
+    private string zoom = "Mouse ScrollWheel";
+    Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
     void FixedUpdate()
     {
+        //TODO: Camera zoom
         move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),0) * speed;
         mouseMove = new Vector3(0, 0, 0);
         position = Input.mousePosition;
         if (position.x <= Screen.width * widthTrigger)
         {
             mouseMove.x = -1;
-            Debug.Log("1");
         }
         else if (position.x >= Screen.width * (1 - widthTrigger))
         {
             mouseMove.x = 1;
-            Debug.Log("2");
         }
             
         if (position.y <= Screen.height * heightTrigger)
         {
             mouseMove.y = -1;
-            Debug.Log(3);
         }
         else if (position.y >= Screen.height * (1 - heightTrigger))
         {
             mouseMove.y = 1;
-            Debug.Log(4);
         }
-            
+        if ((camera.orthographicSize + Input.GetAxisRaw(zoom) * zoomSpeed) >= ZoomBoundLeft && (camera.orthographicSize + Input.GetAxisRaw(zoom) * zoomSpeed) <= ZoomBoundRight)
+        camera.orthographicSize += Input.GetAxisRaw(zoom) * zoomSpeed;
         mouseMove = mouseMove * speed;
         transform.position = transform.position + move + mouseMove;
     }
