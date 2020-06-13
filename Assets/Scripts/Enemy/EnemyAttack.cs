3d52129f71910df67293d5278d21a25c460e216d
AlexDest <condor000123@gmail.com>
2020-06-13 10:40:22 +0300

    Enemy Added
    Movement of enemy to nearest target
    Attack of target (need HP system)

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Enemy"))
+        {
+            GetComponent<EnemyMovement>().SetMove(false);
+            Attack(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        GetComponent<EnemyMovement>().SetMove(true);
+    }
+
+    private void Attack(GameObject ally)
+    {
+        Destroy(ally);
+        //Need HP system
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject mainTarget;
+    private GameObject _currentTarget;
+    private bool _isMove = true;
+
+    void Start()
+    {
+        _currentTarget = mainTarget;
+    }
+
+    void Update()
+    {
+        if (_currentTarget == null) _currentTarget = mainTarget;
+        if (_isMove) {
+            Move(_currentTarget); 
+        } 
+    }
+
+    void Move(GameObject currentTarget)
+    {
+
+        float angle = Vector3.Angle(currentTarget.transform.position - transform.position, Vector3.up);
+
+        Vector3 vectorToTarget = currentTarget.transform.position - transform.position;
+        
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, 
+            Quaternion.FromToRotation(Vector3.up, vectorToTarget), 60*Time.deltaTime);
+        
+        
+        Vector3 direction = currentTarget.transform.position - transform.position;
+        
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        if (target.CompareTag("Ally") || target.CompareTag("Repairable"))
+        {
+            _currentTarget = target;
+        }
+    }
+
+    public void SetMove(bool value)
+    {
+        _isMove = value;
+    }
+}
